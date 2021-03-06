8b584dc91c970b96e7f777f7c8ad79554108429f
Adonis Cipriano <desenvolvimento777@gmail.com>
2021-03-06 14:16:58 -0300

    iniciando no backend

---
+++ iniciando-back-end/data.js
@@ -0,0 +1,39 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animations | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS GRID na pŕatica | Mayk Brito",
+        duration: "36 min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável THIS | Mayk Brito",
+        duration: "48 min",
+        price: "Free"
+    },
+    {
+        id: "ghTrp1x_1As",
+        title: "O que é API? REST e RESTFUL | Mayk Brito",
+        duration: "33 min",
+        price: "Free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: Higher Order Functions | Mayk Brito",
+        duration: "54 min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Git e Github na prática | Mayk Brito",
+        duration: "33 min",
+        price: "Free"
+    },
+    
+]
--- iniciando-back-end/server.js
+++ iniciando-back-end/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data')
 const port = 5000;
 
 server.use(express.static('public'));
@@ -9,17 +10,30 @@ server.use(express.static('public'));
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false
 });
 
 
 
-server.get("/", function(req, res) { // req = request | res = response
-    return res.render("about");
+server.get("/", function(req, res) {
+    const dataAbout = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/42327366?s=460&u=65f0f87120f758ea36d4efab2beed383523b4dc0&v=4",
+        name: "Adonis Cipriano Silveira",
+        role: "Estudante de programação WEB - Rocketseat",
+        description: "Focado em aprender Front-End e Backend pelo curso LaunchBase da  <a href='https://rocketseat.com.br' target='_blank'>Rocketseat</a>",
+        links: [
+            { name: "Github", url: "https://github.com/adoniscs" },
+            { name: "Twitter", url: "https://twitter.com/AdonisCipriano" },
+            { name: "Linkedin", url: "https://www.linkedin.com/in/adonis-cipriano-silveira-811288ab" },
+        ]
+    }
+
+    return res.render("about", { about: dataAbout });
 });
 
 server.get("/portifolio", function(req, res) {
-    return res.render("portifolio");
+    return res.render("portifolio", { items: videos });
 });
  
 server.use(function(req, res) {
